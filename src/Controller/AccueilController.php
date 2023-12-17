a75b72138ec307d861cb4133c77d259461c754eb
MarieFarget <marie.farget4@mail.com>
2023-12-17 18:12:09 +0100

    Ajout de commentaire fonctionnels

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,31 +5,90 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use App\Service\JsonConverter;
 use App\Service\ApiLinker;
+use App\Service\ToolFunctions;
 
 class AccueilController extends AbstractController
 {
     private $jsonConverter;
     private $apiLinker;
+    private $toolFunctions;
 
-    public function __construct(ApiLinker $apiLinker, JsonConverter $jsonConverter) {
+    public function __construct(ApiLinker $apiLinker, JsonConverter $jsonConverter, ToolFunctions $toolFunctions)
+    {
         $this->apiLinker = $apiLinker;
         $this->jsonConverter = $jsonConverter;
+        $this->toolFunctions = $toolFunctions;
     }
 
     #[Route('/', name: 'app_accueil')]
-    public function displayAccueil(): Response
+    public function displayAccueil(Request $request): Response
     {
+        $session = $request->getSession();
+        $token = $session->get('token-session');
+        $payloadData = $this->toolFunctions->getPayload($token);
+        if ($this->toolFunctions->isTokenExpirated($payloadData)) {
+            return $this->redirect('/logout');
+        }
+        if ($payloadData != null) {
+            $userName = $payloadData->username; // Nom de l'utilisateur
+            $userRole = $payloadData->roles[0]; // Rang de l'utilisateur
+
+        } else {
+            $userRole = null;
+            $userName = null;
+        }
+
 
         $allPhotos = json_decode($this->apiLinker->readData("/photos"));
         $allComments = json_decode($this->apiLinker->readData("/commentaires"));
-        
+
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
             'allPhotos' => $allPhotos,
-            'allComments' => $allComments
+            'allComments' => $allComments,
+            'actualUserName' =>  $userName,
+            'actualUserRole' => $userRole
         ]);
     }
+
+    #[Route('/add_comment', name: 'add_comment')]
+    public function addCommentaire(Request $request): Response
+    {
+
+        $textToAdd = $request->request->get('commentaire');
+        $session = $request->getSession();
+        $token = $session->get('token-session');
+        $payloadData = $this->toolFunctions->getPayload($token);
+        if ($this->toolFunctions->isTokenExpirated($payloadData)) {
+            return $this->redirect('/logout');
+        }
+
+
+        if ($payloadData != null) {
+
+            $userId = $this->toolFunctions->getIdByUsername($request->request->get('comment_username'));
+            $senderPostId = intval($request->request->get('comment_photo_id'));
+            $data = [
+                "user_id" => $userId,
+                "description" => $textToAdd
+            ];
+
+            // Formez le JSON à envoyer
+            $jsonData = json_encode($data);
+            var_dump($jsonData);
+            $this->apiLinker->postData('/photos/' . $senderPostId . '/commentaires', $jsonData, $token);
+            return $this->redirect('/');
+        }
+
+        // Gérez ici le cas où le formulaire n'est pas soumis ou s'il y a des erreurs
+
+        return $this->redirect('/login');
+            
+    }
+
 }
---
+++ src/Service/ToolFunctions.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Nelmio\ApiDocBundle\Annotation\Security;
+
+use App\Service\JsonConverter;
+use App\Service\ApiLinker;
+use DateTime;
+
+class ToolFunctions
+{
+    private $apiLinker;
+
+    public function __construct(ApiLinker $apiLinker)
+    {
+        $this->apiLinker = $apiLinker;
+    }
+
+    //Retourne le payload d'un token
+    public function getPayload($token)
+    {
+        if ($token !== null) {
+            $tokenParts = explode('.', $token);
+            $tokenPayload = base64_decode($tokenParts[1]); // Décodage de la partie Payload
+
+            $payloadData = json_decode($tokenPayload);
+            return $payloadData; //Username, password, rôle, expiration(exp)
+        } else {
+            return null;
+        }
+    }
+
+    //Vérifie si le token est expiré
+    public function isTokenExpirated($payloadData)
+    {
+        if ($payloadData) {
+            $expiration = $payloadData->exp;
+
+            // Créer un objet DateTime à partir du timestamp en secondes
+            $expdate = new DateTime("@$expiration");
+            $actualdate = new DateTime();
+
+            // Formater la date d'expiration selon vos besoins
+            $dateExpiration = $expdate->format('Y-m-d H:i:s');
+            $dateActuelle = $actualdate->format('Y-m-d H:i:s');
+
+            // Retourner true si expiré
+            if ($dateActuelle >= $dateExpiration) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Prend en paramètre un username et retourne l'id correspondant
+    public function getIdByUsername($username)
+    {
+        $data = [
+            "username" => $username
+        ];
+
+        // Assurez-vous que le service renvoie une chaîne JSON
+        $jsonResponse = $this->apiLinker->getData("/getIdByUsername/" . $username, null);
+
+        // Décoder la chaîne JSON en tableau PHP
+        $responseData = json_decode($jsonResponse, true);
+
+        // Vérifiez si la clé "id" existe dans le tableau
+        if (isset($responseData['id'])) {
+            return $responseData['id'];
+        }
+
+        // Si la clé "id" n'existe pas, retournez null ou une valeur par défaut selon vos besoins
+        return null;
+    }
+}
