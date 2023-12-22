026fc91538e261edc91fb41aba5607c38c98ff5f
MarieFarget <marie.farget4@mail.com>
2023-12-22 15:55:13 +0100

    Reponse a n'importe quel commentaire fonctionnel

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -37,14 +37,13 @@ class AccueilController extends AbstractController
             $userName = $payloadData->username; // Nom de l'utilisateur
             $userRole = $payloadData->roles[0]; // Rang de l'utilisateur
             $userId = $this->toolFunctions->getIdByUsername($userName);
-
         } else {
             $userRole = null;
             $userName = null;
             $userId = null;
         }
 
-        
+
         $allPhotos = json_decode($this->apiLinker->readData("/photos"));
         $allComments = json_decode($this->apiLinker->readData("/commentaires"));
 
@@ -91,7 +90,60 @@ class AccueilController extends AbstractController
         // Gérez ici le cas où le formulaire n'est pas soumis ou s'il y a des erreurs
 
         return $this->redirect('/login');
-            
     }
 
+    #[Route('/rep_comment/{idParentComment}/{userToAnnote}', name: 'rep_comment')]
+    public function addReponse(Request $request, $idParentComment, $userToAnnote): Response
+    {
+
+        $textToAdd = $request->request->get('commentaire');
+        $session = $request->getSession();
+        $token = $session->get('token-session');
+        $payloadData = $this->toolFunctions->getPayload($token);
+        if ($this->toolFunctions->isTokenExpirated($payloadData)) {
+            return $this->redirect('/logout');
+        }
+
+        if ($payloadData != null) {
+            $userName = $payloadData->username; // Nom de l'utilisateur
+            $userRole = $payloadData->roles[0]; // Rang de l'utilisateur
+            $userId = $this->toolFunctions->getIdByUsername($userName);
+        } else {
+            $userRole = null;
+            $userName = null;
+            $userId = null;
+            return $this->redirect('/login');
+        }
+
+        $allPhotos = json_decode($this->apiLinker->readData("/photos"));
+        $allComments = json_decode($this->apiLinker->readData("/commentaires"));
+
+        if ($payloadData != null) {
+
+            $userId = $this->toolFunctions->getIdByUsername($request->request->get('comment_username'));
+            $senderPostId = intval($request->request->get('comment_photo_id'));
+            if ($userToAnnote != 'noUserToAnnote') {
+                $descript = ('@'.$userToAnnote.' '.$textToAdd);
+            }
+            else {
+                $descript = $textToAdd;
+            }
+            $data = [
+                "user_id" => $userId,
+                "description" => $descript,
+                "parent_comment_id" => $idParentComment
+            ];
+
+            // Formez le JSON à envoyer
+            $jsonData = json_encode($data);
+            var_dump($jsonData);
+            $this->apiLinker->postData('/photos/' . $senderPostId . '/commentaires', $jsonData, $token);
+
+            return $this->redirect('/');
+        }
+
+        // Gérez ici le cas où le formulaire n'est pas soumis ou s'il y a des erreurs
+
+        return $this->redirect('/login');
+    }
 }
